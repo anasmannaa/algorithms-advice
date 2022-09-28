d29c6c9c0b4e6ece8d717e84fa60c0ebaa94edec
Anas Mannaa <30526159+anasmannaa@users.noreply.github.com>
2022-09-28 09:12:18 +0400

    Create 16.cpp

---
+++ 02/16.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void printWordsFromAAAtoZZZ() {
+    string word = "";
+    for (int i = 65; i <= 90; i++) {
+        for (int j = 65; j <= 90; j++) {
+            for (int k = 65; k <= 90; k++) {
+                word += char(i);
+                word += char(j);
+                word += char(k);
+                cout << word << endl;
+                word = "";
+            }
+        }
+    }
+}
+
+int main()
+{
+    printWordsFromAAAtoZZZ();
+    return 0;
+}
+
